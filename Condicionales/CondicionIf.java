cfb863eba066926395f23892dd604fee35284f0b
Tania <you@example.com>
2021-02-23 19:12:33 +0100

    if

---
+++ Condicionales/CondicionIf.java
@@ -0,0 +1,14 @@
+package Condicionales;
+
+public class CondicionIf {
+	public static void main(String[] args) {
+		int n1=11;
+		int n2=2;
+		if (n1%n2==0) {
+			System.out.println("es par");
+		}else {
+			System.out.println("es impar");
+		}
+				
+	}
+}
